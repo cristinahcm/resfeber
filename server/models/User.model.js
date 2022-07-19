2d6554a97fcd92ebbe1824aea9bbe585c6eafc47
Jessica Fernandez <jferasef@gmail.com>
2022-07-19 19:40:41 +0200

    auth

---
+++ server/models/User.model.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    picture: {
+      type: Image,
+    },
+    password: String,
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
